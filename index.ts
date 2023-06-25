ad6c4d724821810a694535faed62222babdaeac7
lsm <lsm@thetelos.net>
2023-06-25 11:59:43 +0900

    chore: 타입 alias 사용, 정규식으로 쉽게 풀 문제를 오버?킬 해버리기

--- index.ts
+++ index.ts
@@ -57,3 +57,92 @@ console.log(tt);
 // };
 
 /*********************************************************************************************************************************/
+
+// Q. literal 타입을 활용하여 함수 만들기
+// 1. 가위 or 바위 or 보 중 1개 입력가능
+// 2. 가위 or 바위 or 보 만 들어올 수 있는 array를 return 해야함
+
+// const testFunction = (
+//   choice: "rock" | "paper" | "scissors"
+// ): ("rock" | "paper" | "scissors")[] => {
+//   return [choice];
+// };
+
+// testFunction("rock");
+
+/*********************************************************************************************************************************/
+
+// Q. object 자료속 함수에 타입 지정하기.
+// 1. plusOne이라는 속성은 함수여야하고, 숫자를 넣어서 숫자를 뱉는 함수여야합니다.
+// 2. changeName이라는 속성은 함수여야하고, 아무것도 return하면 안됩니다.
+// 3. type 키워드를 쓰든 말든 알아서 합시다.
+
+// type TestUserInfoType = {
+//   name: string;
+//   age: number;
+//   plusOne: (x: number) => number;
+//   changeName: () => void;
+// };
+
+// const testUserInfo: TestUserInfoType = {
+//   name: "kim",
+//   age: 30,
+//   plusOne: (x) => {
+//     return x + 1;
+//   },
+//   changeName: () => {
+//     console.log("hello");
+//   },
+// };
+
+// testUserInfo.plusOne(1);
+// testUserInfo.changeName();
+
+/*********************************************************************************************************************************/
+
+// Q. 다음 함수2개를 만들어보고 타입까지 정의해보십시오.
+// 1. cutZero()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 맨 앞에 '0' 문자가 있으면 제거하고 문자 type으로 return 해줍니다.
+// 2. removeDash()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 대시기호 '-' 가 있으면 전부 제거해주고 그걸 숫자 type으로 return 해줍니다.
+// 3. 함수에 타입지정시 type alias를 꼭 써보도록 합시다.
+
+// 정규식으로 풀면 쉬운걸 엄청 배배꼬아서 풀어버린 나는 바보
+
+// type CutZeroType = (x: string) => string;
+
+// const cutZero: CutZeroType = (x) => {
+//   let arr = x.split("");
+
+//   if (arr[0] === "0") {
+//     let sliceTo = 0;
+//     for (let i = 0; i <= arr.length; i++) {
+//       if (arr[i] !== "0") break;
+//       sliceTo = i + 1;
+//     }
+//     arr = arr.slice(sliceTo);
+//   }
+
+//   console.log(arr.join(""));
+//   return arr.join("");
+// };
+
+// cutZero("000000앞빵처리000000");
+
+// type RemoveDashType = (x: string) => number;
+
+// const removeDash: RemoveDashType = (x) => {
+//   const arr = x.split("");
+//   const filterDash: string[] = [];
+
+//   arr.forEach((val, _) => {
+//     if (val !== "-") {
+//       filterDash.push(val);
+//     }
+//   });
+
+//   console.log(parseInt(filterDash.join(""), 10));
+//   return parseInt(filterDash.join(""), 10);
+// };
+
+// removeDash("010-1234-5678");
+
+/*********************************************************************************************************************************/
